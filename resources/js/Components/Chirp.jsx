c5066223b5c56fc46f3dff2f3c8aef0ee1be7608
verboddo <verboddo@gmail.com>
2023-10-08 16:02:19 +0200

    added delete button to delete chirp

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -5,23 +5,23 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { useForm, usePage } from '@inertiajs/react';
- 
+
 dayjs.extend(relativeTime);
- 
+
 export default function Chirp({ chirp }) {
     const { auth } = usePage().props;
- 
+
     const [editing, setEditing] = useState(false);
- 
+
     const { data, setData, patch, clearErrors, reset, errors } = useForm({
         message: chirp.message,
     });
- 
+
     const submit = (e) => {
         e.preventDefault();
         patch(route('chirps.update', chirp.id), { onSuccess: () => setEditing(false) });
     };
- 
+
     return (
         <div className="p-6 flex space-x-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -47,6 +47,9 @@ export default function Chirp({ chirp }) {
                                 <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
                                     Edit
                                 </button>
+                                <Dropdown.Link as="button" href={route('chirps.destroy', chirp.id)} method="delete">
+                                    Delete
+                                </Dropdown.Link>
                             </Dropdown.Content>
                         </Dropdown>
                     }
